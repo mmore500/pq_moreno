eccff5c309dff6d3fcfdba9b9f5f6749a103ee34
Matthew Moreno <mmore500@gmail.com>
2016-09-16 15:52:18 -0700

    Add toString to Pair.java

--- Pair.java
+++ Pair.java
@@ -1,15 +1,20 @@
 /**
  * A wrapper class for priority queue elements
- * 
+ *
  * @author americachambers
  *
  */
 public class Pair<P, E> {
 	public P priority;
 	public E element;
-	
+
 	public Pair(P p, E e) {
 		priority = p;
 		element = e;
 	}
+
+	public String toString() {
+		return "(priority: " + this.p + ", element: " + this.e + ")";
+	}
+
 }
